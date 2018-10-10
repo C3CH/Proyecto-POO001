32ca858898e258dea4ae89b334241ad8e7dd4762
C3CH <39921516+C3CH@users.noreply.github.com>
2018-10-10 08:55:28 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+void inicializar(float array[],string telas[],string prendas[]){
+    float Precio;
+    for (int i=0; i<cantidad_telas;i++){
+        cin>>telas[i];
+        for (int e=0;e<columnas;e++){
+            cin>>prendas[e];
+            cin>>Precio;
+            array[i][e]=prendas[e]*Precio;
+        }
+    }
+}
+
+float calculo_tiempos(){
+    
+}
+
+float calculo_materiaprima(){
+    
+}
+
+float calculo_costos(){
+    
+}
+
+int const cantidad_telas=3;
+int const cantidad_prendas=3;
+
+main(){
+    float array[cantidad_telas][cantidad_prendas]={0};
+    string telas[cantidad_telas];
+    string prendas[cantidad_prendas]={polos, camisas, cuellos};
+    
+    inicializar(array,telas,prendas);
+    
+    
+}
